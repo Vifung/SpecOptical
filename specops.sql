d01b95b089403c6022189ddc555b0a9729b5201e
Vifung <Vifung@users.noreply.github.com>
2016-03-01 10:33:22 -0800

    Set some things to null
    Set a few rows in users table to have ability to be null

--- specops.sql
+++ specops.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.10
+-- version 4.4.10
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 01, 2016 at 06:58 PM
--- Server version: 5.5.38
--- PHP Version: 5.6.2
+-- Generation Time: Mar 01, 2016 at 07:30 PM
+-- Server version: 5.5.42
+-- PHP Version: 5.6.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -21,19 +21,22 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `cart` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `state` varchar(30) NOT NULL,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `total` decimal(15,2) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `cart`
 --
 
 INSERT INTO `cart` (`id`, `state`, `time`, `total`) VALUES
-(1, 'cancelled', '2016-02-05 05:39:33', 12.95),
-(2, 'started', '2016-02-19 00:13:05', 0.00);
+(1, 'cancelled', '2016-02-05 05:39:33', '12.95'),
+(2, 'started', '2016-02-19 00:13:05', '0.00'),
+(3, 'cancelled', '2016-03-01 18:16:23', '0.00'),
+(4, 'cancelled', '2016-03-01 18:17:55', '0.00'),
+(5, 'started', '2016-03-01 18:20:20', '0.00');
 
 -- --------------------------------------------------------
 
@@ -42,7 +45,7 @@ INSERT INTO `cart` (`id`, `state`, `time`, `total`) VALUES
 --
 
 CREATE TABLE `cart_product` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
   `cart_id` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
@@ -56,7 +59,7 @@ CREATE TABLE `cart_product` (
 --
 
 CREATE TABLE `product` (
-`id` int(225) NOT NULL,
+  `id` int(225) NOT NULL,
   `SKU` varchar(100) NOT NULL,
   `style` varchar(100) NOT NULL,
   `colour` varchar(100) NOT NULL,
@@ -70,15 +73,15 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id`, `SKU`, `style`, `colour`, `type`, `unit_price`, `img`) VALUES
-(1, '12354653', 'classic', 'black', 'prescription', 215.00, './images/Classic_Burberry_BE1156.jpg'),
-(2, '08099988', 'classic', 'black', 'prescription', 107.00, './images/Classic_Burberry_BE1156.jpg'),
-(3, '99934678', 'classic', 'black', 'prescription', 205.00, './images/Classic_Burberry_BE1156.jpg'),
-(4, '77987399', 'trendy', 'black', 'prescription', 150.00, './images/Classic_Burberry_BE1156.jpg'),
-(5, '77809099', 'trendy', 'black', 'prescription', 107.00, './images/Classic_Burberry_BE1156.jpg'),
-(6, '76632199', 'trendy', 'black', 'prescription', 157.00, './images/Classic_Burberry_BE1156.jpg'),
-(7, '11399823', 'nerdy', 'black', 'prescription', 300.00, './images/Classic_Burberry_BE1156.jpg'),
-(8, '11299900', 'nerdy', 'black', 'prescription', 208.00, './images/Classic_Burberry_BE1156.jpg'),
-(9, '00398922', 'nerdy', 'black', 'prescription', 285.00, './images/Classic_Burberry_BE1156.jpg');
+(1, '12354653', 'classic', 'black', 'prescription', '215.00', './images/Classic_Burberry_BE1156.jpg'),
+(2, '08099988', 'classic', 'black', 'prescription', '107.00', './images/Classic_Burberry_BE1156.jpg'),
+(3, '99934678', 'classic', 'black', 'prescription', '205.00', './images/Classic_Burberry_BE1156.jpg'),
+(4, '77987399', 'trendy', 'black', 'prescription', '150.00', './images/Classic_Burberry_BE1156.jpg'),
+(5, '77809099', 'trendy', 'black', 'prescription', '107.00', './images/Classic_Burberry_BE1156.jpg'),
+(6, '76632199', 'trendy', 'black', 'prescription', '157.00', './images/Classic_Burberry_BE1156.jpg'),
+(7, '11399823', 'nerdy', 'black', 'prescription', '300.00', './images/Classic_Burberry_BE1156.jpg'),
+(8, '11299900', 'nerdy', 'black', 'prescription', '208.00', './images/Classic_Burberry_BE1156.jpg'),
+(9, '00398922', 'nerdy', 'black', 'prescription', '285.00', './images/Classic_Burberry_BE1156.jpg');
 
 -- --------------------------------------------------------
 
@@ -87,7 +90,7 @@ INSERT INTO `product` (`id`, `SKU`, `style`, `colour`, `type`, `unit_price`, `im
 --
 
 CREATE TABLE `transactions` (
-`id` int(225) NOT NULL,
+  `id` int(225) NOT NULL,
   `user_id` int(225) NOT NULL,
   `glasses_id` int(225) NOT NULL,
   `status` varchar(500) NOT NULL,
@@ -101,18 +104,18 @@ CREATE TABLE `transactions` (
 --
 
 CREATE TABLE `users` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `email` varchar(500) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `address1` varchar(100) NOT NULL,
+  `address1` varchar(100) DEFAULT NULL,
   `address2` varchar(500) DEFAULT NULL,
-  `country` varchar(500) NOT NULL,
-  `staProv` varchar(500) NOT NULL,
-  `city` varchar(500) NOT NULL,
-  `zipPo` varchar(6) NOT NULL,
+  `country` varchar(500) DEFAULT NULL,
+  `staProv` varchar(500) DEFAULT NULL,
+  `city` varchar(500) DEFAULT NULL,
+  `zipPo` varchar(6) DEFAULT NULL,
   `fname` text NOT NULL,
   `lname` text NOT NULL,
-  `phoneNum` int(10) NOT NULL
+  `phoneNum` int(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -123,31 +126,37 @@ CREATE TABLE `users` (
 -- Indexes for table `cart`
 --
 ALTER TABLE `cart`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `cart_product`
 --
 ALTER TABLE `cart_product`
- ADD PRIMARY KEY (`id`), ADD KEY `product_id` (`product_id`), ADD KEY `cart_id` (`cart_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `cart_id` (`cart_id`);
 
 --
 -- Indexes for table `product`
 --
 ALTER TABLE `product`
- ADD PRIMARY KEY (`id`), ADD KEY `id` (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
 
 --
 -- Indexes for table `transactions`
 --
 ALTER TABLE `transactions`
- ADD PRIMARY KEY (`user_id`,`glasses_id`,`id`), ADD KEY `id` (`id`), ADD KEY `glasses_id` (`glasses_id`);
+  ADD PRIMARY KEY (`user_id`,`glasses_id`,`id`),
+  ADD KEY `id` (`id`),
+  ADD KEY `glasses_id` (`glasses_id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
- ADD PRIMARY KEY (`id`), ADD KEY `id` (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -157,27 +166,27 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `cart_product`
 --
 ALTER TABLE `cart_product`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-MODIFY `id` int(225) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+  MODIFY `id` int(225) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-MODIFY `id` int(225) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(225) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
@@ -186,12 +195,12 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 -- Constraints for table `cart_product`
 --
 ALTER TABLE `cart_product`
-ADD CONSTRAINT `cart_product_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-ADD CONSTRAINT `cart_product_ibfk_2` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `cart_product_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `cart_product_ibfk_2` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `transactions`
 --
 ALTER TABLE `transactions`
-ADD CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-ADD CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`glasses_id`) REFERENCES `product` (`id`);
+  ADD CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`glasses_id`) REFERENCES `product` (`id`);
